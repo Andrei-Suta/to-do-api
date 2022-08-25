4371decaf4f3107e308e605bbb2fa3ba8d3f9964
Andrei Radu Suta <andreiradusuta@gmail.com>
2022-08-25 16:42:38 +0300

    added authentication module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { User } from "src/user/user.entity";
+import { AuthDTO } from "./auth.dto";
+import { AuthService } from "./auth.service";
+
+@Controller("auth")
+export class AuthController {
+	public constructor(
+		private authService: AuthService
+	) { }
+
+	@Post("register")
+	private register(@Body() authDTO: AuthDTO): Promise<User | never> {
+		return this.authService.register(authDTO);
+	}
+
+	@Post("login")
+	private login(@Body() authDTO: AuthDTO): Promise<string | never> {
+		return this.authService.login(authDTO);
+	}
+
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
+
+import { User } from "src/user/user.entity";
+import { Repository } from "typeorm";
+import { AuthDTO } from "./auth.dto";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class AuthService {
+	public constructor(
+		@InjectRepository(User)
+		private usersRepository: Repository<User>,
+		private jwtService: JwtService,
+		private configService: ConfigService
+	) { }
+
+	public async register(authDto: AuthDTO): Promise<User | never> {
+		let user: User = await this.usersRepository.findOne({ where: { email: authDto.email } });
+		if (user) {
+			throw new HttpException("An account with this email adress has already been registered", HttpStatus.CONFLICT);
+		}
+		user = new User();
+		user.email = authDto.email;
+		user.password = authDto.password;
+		user.password = await this.hashPassword(user);
+		return await this.usersRepository.save(user);
+	}
+
+	public async login(authDTO: AuthDTO): Promise<string | never> {
+		const user: User = await this.usersRepository.findOne({ where: { email: authDTO.email } });
+		if (!user) {
+			throw new HttpException("No account registered with the provided email", HttpStatus.NOT_FOUND);
+		}
+		const isMatch = await bcrypt.compare(authDTO.password, user.password);
+		if (!isMatch) {
+			throw new HttpException("Wrong password", HttpStatus.NOT_FOUND);
+		}
+		return this.generateToken(user);
+	}
+
+	public async hashPassword(user: User): Promise<string> {
+		const salt = await bcrypt.genSalt();
+		return await bcrypt.hash(user.password, salt);
+	}
+
+	public generateToken(user: User): string {
+		const tokenDTO = {
+			id: user.id,
+			email: user.email
+		};
+		return this.jwtService.sign(tokenDTO, { secret: this.configService.get("JWT_KEY") });
+		//, { secret: this.configService.get("JWT_KEY") }
+	}
+}
